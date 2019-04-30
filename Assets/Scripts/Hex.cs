0edf6e7fbe4f32bcb868eca9171abfe4d11415f9
nikbr <nbryliov@gmail.com>
2019-04-30 05:47:31 -0400

    Going to editor now creates a 10 x 10 hexagonal grid.

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex {
+	
+	public Hex(int q, int r){
+		Q = q;
+		R = r;
+		S = -(q+r);
+	}
+
+	public readonly int Q;
+	public readonly int R;
+	public readonly int S;
+
+	readonly static float RADIUS = 1f;
+	readonly static float HEIGHT = RADIUS*2;
+	readonly static float WIDTH = HEIGHT * Mathf.Sqrt(3)/2;
+
+	public Vector3 Position(){
+		float horizontalSpacing = WIDTH;
+		float verticalSpacing = HEIGHT*0.75f;
+		return new Vector3(horizontalSpacing*(Q + R/2f),0,verticalSpacing*R);
+	}
+}
---
+++ Assets/Scripts/HexMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		defaultMap();
+	}
+	public GameObject HexPrefab;
+	// Update is called once per frame
+	public void defaultMap(){
+		for(int col=0;col<10;col++){
+			for(int row =0;row<10;row++){
+				Hex hex = new Hex(col, row);
+				Instantiate(HexPrefab, hex.Position(), Quaternion.identity, this.transform);
+			}
+		}
+	}
+}
